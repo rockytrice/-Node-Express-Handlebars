b6fd06e9d244ec037db627b924f7b90f980d19c1
Roderick Trice <rocky_trice@yahoo.com>
2018-08-24 22:04:55 -0400

    finished up the sql statement functions

--- Car/config/orm.js
+++ Car/config/orm.js
@@ -5,37 +5,53 @@ var connection = require("../config/connection.js");
 
 // Object for all our SQL statement functions.
 var orm = {
-    selectAll: function(tableInput, callback) {
-      var queryString = "SELECT * FROM " + tableInput + ";";
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        callback(result);
-      });
+    selectAll: function (tableInput, callback) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
     },
-    insertOne: function(table,cols,vals,callback){
-var queryString = "INSERT INTO " + table;
-queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-    console.log(queryString);
+    insertOne: function (table, cols, vals, callback) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
 
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
+        console.log(queryString);
 
-      callback(result);
-    });
-    }
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
 
+            callback(result);
+        });
+    },
 
-
-
-
-
+    updateOne: function(table, objColVals, condition, callback) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          callback(result);
+        });
+      },
 }
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
