dd00ef9854136b937b9d12672b7599177ff11974
Roderick Trice <rocky_trice@yahoo.com>
2018-08-24 21:45:41 -0400

    adding selectAll statement function

--- Car/config/orm.js
+++ Car/config/orm.js
@@ -1,3 +1,23 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
 
 
 
+// Object for all our SQL statement functions.
+var orm = {
+    selectAll: function(tableInput, callback) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        callback(result);
+      });
+    },
+
+
+
+
+
+
+}
