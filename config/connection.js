d464b991b82f03a3aea7f1a10ed53cd0909e219d
Roderick Trice <rocky_trice@yahoo.com>
2018-08-27 14:38:20 -0400

    jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "cars_db"
-});
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password:"root",
+    database:"cars_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
